ff8341349b2030b17a8fd875306807e30a734566
Prasad Adsul <PRASADADSUL81@GMAIL.COM>
2023-04-27 23:59:02 +0530

    Add files via upload

---
+++ Sales Management Portal/Home.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainMaster.master" AutoEventWireup="true"
+    CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <link href="Content/landing-page.css" rel="stylesheet" />
+
+    <div class="intro-header">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <marquee><h2><%=Announcements.ToString() %></h2></marquee>
+                    <div class="intro-message">
+                        <h1>Hello <%=Session["username"].ToString() %></h1>
+                        <h3>Welcome to Sales Management</h3>
+                        <hr class="intro-divider" />
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</asp:Content>
+
---
+++ Sales Management Portal/Home.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Home : System.Web.UI.Page
+{
+    public StringBuilder Announcements = new StringBuilder();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Convert.ToBoolean(Session["IsAuth"]))
+            {
+                if (Session["role"].ToString().ToUpper() == "SALES")
+                {
+                    try
+                    {
+                        SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SalesConnectionString"].ConnectionString);
+                        SqlCommand cmd = new SqlCommand("select Message from Announcement_table where datetime =CONVERT(CHAR(23),CONVERT(DATETIME,GetDate(),101),121)", conn);
+
+                        try
+                        {
+                            Announcements = new StringBuilder();
+                            conn.Open();
+                            SqlDataReader dr = cmd.ExecuteReader();
+                            int i = 1;
+                            while (dr.Read())
+                            {
+                                Announcements.Append(i++);
+                                Announcements.Append(")");
+                                Announcements.Append(dr["Message"].ToString());
+                                Announcements.Append("&nbsp;&nbsp;");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                        finally
+                        {
+                            conn.Close();
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+                
+            }
+        }
+        catch (Exception EX)
+        {
+
+            Response.Redirect("Login.aspx");
+        }
+        
+    }
+}
